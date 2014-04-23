4c9fe2b4da6bebb40785d1fda12216463d853145
dantezhu <dantezhu@qq.com>
2014-04-23 23:15:12 +0800

    优化server和bin的写法

--- examples/network/server.py
+++ examples/network/server.py
@@ -17,33 +17,35 @@ logger.addHandler(logging.FileHandler('s.log'))
 logger.setLevel(logging.DEBUG)
 
 
-def handle(socket, address):
-    stream = Stream(socket)
-    status = dict(
-        alive=True
-    )
-    while status['alive']:
-        # 必须要启动一个新的greenlet，在greenlet里面执行readline
-        # 否则会有内存泄漏
-        def spawn_read():
-            buf = stream.read_with_checker(bintp.from_buf)
-            print buf
-            logger.info('closed: %s', stream.closed())
-            logger.info('sock: %s', stream.sock)
+class Connection(object):
 
-            if stream.closed():
-                status['alive'] = False
-                logger.info('client closed')
-                # 说明客户端断掉链接了
-                return
+    def __init__(self, sock, address):
+        self.stream = Stream(sock)
+        self.process()
 
-        logger.error('status: %s', status)
-        t = gevent.spawn(spawn_read)
-        t.join()
+    def process(self):
+        while not self.stream.closed():
+            # 必须要启动一个新的greenlet，在greenlet里面执行readline
+            # 否则会有内存泄漏
+            def spawn_read():
+                message = self.stream.read_with_checker(bintp.from_buf)
+                if self.stream.closed():
+                    print 'client closed'
+                    # 说明客户端断掉链接了
+                    return
+                print "message, len: %s, content: %r" % (len(message), message)
+
+                ret, tp = bintp.from_buf(message)
+                tp.cmd += 1
+                self.stream.write(tp.pack())
+
+            t = gevent.spawn(spawn_read)
+            t.join()
 
 
-    # 即使不调用close，handle结束也会自动关闭
-    #socket.close()
+        # 即使不调用close，handle结束也会自动关闭
+        #sock.close()
 
-server = StreamServer(('127.0.0.1', 7777), handle)
+
+server = StreamServer(('127.0.0.1', 7777), Connection)
 server.serve_forever()
